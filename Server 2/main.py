d6fe4073ebfea81563807f30fbc5183163d8c9c4
Sanket <sankyjamadar@gmail.com>
2024-04-26 21:40:43 +0530

    api commented out

--- Server 2/main.py
+++ Server 2/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from torchvision import models, transforms
 from PIL import Image
+
 import torch
 import base64
 import gzip
@@ -69,36 +70,36 @@ def predict():
 
 
 ###################################### FERTILIZER  PREDICTION  jOBLIB  ################################## 
-with open('trained_model.joblib','rb') as f1:
-    model_fertilizer=joblib.load(f1)
+# with open('trained_model.joblib','rb') as f1:
+#     model_fertilizer=joblib.load(f1)
 
 ###################################### FERTILIZER  PREDICTION  API  ##################################
-@app.route('/predict/fertilizer',methods=['POST'])
-def predict_fertilizer():
-    data = request.get_json()
+# @app.route('/predict/fertilizer',methods=['POST'])
+# def predict_fertilizer():
+#     data = request.get_json()
 
-    temperature = data['Temperature']
-    humidity = data['Humidity']
-    moisture = data['Moisture']
-    soil_type = data['Soil Type']
-    crop_type = data['Crop Type']
-    nitrogen = data['Nitrogen']
-    potassium = data['Potassium']
-    phosphorous = data['Phosphorous']
+#     temperature = data['Temperature']
+#     humidity = data['Humidity']
+#     moisture = data['Moisture']
+#     soil_type = data['Soil Type']
+#     crop_type = data['Crop Type']
+#     nitrogen = data['Nitrogen']
+#     potassium = data['Potassium']
+#     phosphorous = data['Phosphorous']
 
-    encoded_soil_type = encoded_categories['Soil Type'].get(soil_type, None)
-    encoded_crop_type = encoded_categories['Crop Type'].get(crop_type, None)
+#     encoded_soil_type = encoded_categories['Soil Type'].get(soil_type, None)
+#     encoded_crop_type = encoded_categories['Crop Type'].get(crop_type, None)
 
-    if encoded_soil_type is None:
-        return jsonify({'error': f"'{soil_type}' not found in encoded categories for Soil Type."}), 400
-    if encoded_crop_type is None:
-        return jsonify({'error': f"'{crop_type}' not found in encoded categories for Crop Type."}), 400
+#     if encoded_soil_type is None:
+#         return jsonify({'error': f"'{soil_type}' not found in encoded categories for Soil Type."}), 400
+#     if encoded_crop_type is None:
+#         return jsonify({'error': f"'{crop_type}' not found in encoded categories for Crop Type."}), 400
 
-    prediction = model_fertilizer.predict([[temperature, humidity, moisture, encoded_soil_type, encoded_crop_type, nitrogen, potassium, phosphorous]])
+#     prediction = model_fertilizer.predict([[temperature, humidity, moisture, encoded_soil_type, encoded_crop_type, nitrogen, potassium, phosphorous]])
 
-    predicted_fertilizer_name = prediction[0]
+#     predicted_fertilizer_name = prediction[0]
 
-    return jsonify({'predicted_fertilizer_name': predicted_fertilizer_name})
+#     return jsonify({'predicted_fertilizer_name': predicted_fertilizer_name})
 
 ###################################### FERTILIZER  PREDICTION  API  ##################################
 
